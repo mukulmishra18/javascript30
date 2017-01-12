1de66f2b8b167dd72dc214eafc0095409f9a8476
Mukul Mishra <mkmishra1895@gmail.com>
2017-01-12 20:18:19 +0530

    Project 1: A simple Javascript drum kit, that can play sounds

---
+++ day1/day1.js
@@ -0,0 +1,22 @@
+window.addEventListener('keydown', function(e) {
+	
+	const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key = "${e.keyCode}"`);
+	if(!audio) {
+		return;
+	}
+
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
+
+});
+
+function removeTransition(e) {
+	//console.log(e);
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
